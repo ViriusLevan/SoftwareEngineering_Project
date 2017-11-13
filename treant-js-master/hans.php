962a953578b31236f4ae4ac20f60ddf34f067cb5
hanschrs <hans_christian@ymail.com>
2017-11-13 23:21:08 +0700

    adding tree library

---
+++ treant-js-master/hans.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width">
+    <title> ES </title>
+    <link rel="stylesheet" href="Treant.css">
+    <link rel="stylesheet" href="es-tree.css">
+    
+</head>
+<body>
+    <div class="chart" id="basic-example"></div>
+    <script src="vendor/raphael.js"></script>
+    <script src="Treant.js"></script>
+    
+    <script src="es-tree.js"></script>
+    <script>
+        new Treant( chart_config );
+    </script>
+</body>
+</html>
